cc05968d36dd66e6312beac2d0154718af654cf8
Veronika Doubkova <doubkova.veronika264@seznam.cz>
2018-11-20 13:42:19 +0100

    Upraven Tancik

--- src/tanks/Tancik.java
+++ src/tanks/Tancik.java
@@ -3,25 +3,35 @@ package tanks;
 import java.util.Vector;
 
 import robocode.AdvancedRobot;
+import robocode.HitRobotEvent;
+import robocode.ScannedRobotEvent;
 
-public class tancik {
+public class Tancik extends AdvancedRobot {
 
-    public class Tancik extends AdvancedRobot{
+    public Vector<Gene> genesRun;
+    public Vector<Gene> genesOnScanned;
+    public Vector<Gene> genesOnHit;
 
-        public Vector<Gene> genes;
-        public Vector<Gene> genesRun;
-        public Vector<Gene> genesOnScanned;
-        public Vector<Gene> genesOnHit;
-        public Tancik(Vector<Gene> genes) {
-
-            this.genesRun = genes;
-            this.genesOnScanned = genes;
-            this.genesOnHit = genes;
-            this.genes = genes;
+    public Tancik(Vector<Gene> genes) {
+        genesRun = new Vector<>();
+        for (int i = 0; i < Config.getPercRun() * Config.getNumOfGenes(); i++) {
+            genesRun.add(genes.remove(0));
 
         }
 
-        public void run() {
+        genesOnScanned = new Vector<>();
+        for (int i = 0; i < Config.getPercOnScanned() * Config.getNumOfGenes(); i++) {
+            genesOnScanned.add(genes.remove(0));
+        }
+        genesOnHit = new Vector<>();
+        for (int i = 0; i < Config.getPercOnHit() * Config.getNumOfGenes(); i++) {
+            genesOnHit.add(genes.remove(0));
+        }
+    }
+
+    public void run() {
+
+        while (true) {
 
             for (Gene gene : genesRun) {
                 switch (gene.type) {
@@ -38,104 +48,96 @@ public class tancik {
                         setTurnLeft(360 * gene.value);
                         break;
                     case 4:
-                        setEnergy((int)(50 * gene.value));
+                        setTurnGunRight(360 * gene.value);
                         break;
                     case 5:
                         setFire(5 * gene.value);
                         break;
                     case 6:
-                        setBearing(180 * gene.value);
+                        setTurnGunLeft(360 * gene.value);
                         break;
                     case 7:
-                        setDistance(10 * gene.value);
+                        //setDistance(10 * gene.value);
                         break;
                     case 8:
-                        setMaxVelocity (5* gene.value);
+                        setMaxVelocity(5 * gene.value);
                         break;
                 }
 
             }
-        }
-
-        public void onScannedRobot() {
-
-            for (Gene gene : genesOnScanned) {
-                switch (gene.type) {
-                    case 0:
-                        setAhead(-40000 * gene.value);
-                        break;
-                    case 1:
-                        setAhead(40000 * gene.value);
-                        break;
-                    case 2:
-                        setTurnRight(360 * gene.value);
-                        break;
-                    case 3:
-                        setTurnLeft(360 * gene.value);
-                        break;
-                    case 4:
-                        setEnergy((int)(50 * gene.value));
-                        break;
-                    case 5:
-                        setFire(5 * gene.value);
-                        break;
-                    case 6:
-                        setBearing(180 * gene.value);
-                        break;
-                    case 7:
-                        setDistance(10 * gene.value);
-                        break;
-                    case 8:
-                        setMaxVelocity (5* gene.value);
-                        break;
-                }
-            }
-        }
-
-        public void onHitRobot() {
-
-            for (Gene gene : genesOnHit) {
-                switch (gene.type) {
-                    case 0:
-                        setAhead(-40000 * gene.value);
-                        break;
-                    case 1:
-                        setAhead(40000 * gene.value);
-                        break;
-                    case 2:
-                        setTurnRight(360 * gene.value);
-                        break;
-                    case 3:
-                        setTurnLeft(360 * gene.value);
-                        break;
-                    case 4:
-                        setEnergy((int)(50 * gene.value));
-                        break;
-                    case 5:
-                        setFire(5 * gene.value);
-                        break;
-                    case 6:
-                        setBearing(180 * gene.value);
-                        break;
-                    case 7:
-                        setDistance(10 * gene.value);
-                        break;
-                    case 8:
-                        setMaxVelocity (5* gene.value);
-                        break;
-                }
-            }
-        }
-        private void setEnergy(int i) {
-            // TODO Auto-generated method stub
-
-        }
-        private void setDistance(double d) {
-            // TODO Auto-generated method stub
-
-        }
-        private void setBearing(double d) {
-            // TODO Auto-generated method stub
         }
     }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+
+
+        for (Gene gene : genesOnScanned) {
+            switch (gene.type) {
+                case 0:
+                    setAhead(-40000 * gene.value);
+                    break;
+                case 1:
+                    setAhead(40000 * gene.value);
+                    break;
+                case 2:
+                    setTurnRight(360 * gene.value);
+                    break;
+                case 3:
+                    setTurnLeft(360 * gene.value);
+                    break;
+                case 4:
+                    setTurnGunRight(360 * gene.value);
+                    break;
+                case 5:
+                    setFire(5 * gene.value);
+                    break;
+                case 6:
+                    setTurnGunLeft(360 * gene.value);
+                    break;
+                case 7:
+                    //etDistance(10 * gene.value);
+                    break;
+                case 8:
+                    setMaxVelocity(5 * gene.value);
+                    break;
+            }
+        }
+    }
+
+    public void onHitRobot(HitRobotEvent event) {
+
+        for (Gene gene : genesOnHit) {
+            switch (gene.type) {
+                case 0:
+                    setAhead(-40000 * gene.value);
+                    break;
+                case 1:
+                    setAhead(40000 * gene.value);
+                    break;
+                case 2:
+                    setTurnRight(360 * gene.value);
+                    break;
+                case 3:
+                    setTurnLeft(360 * gene.value);
+                    break;
+                case 4:
+                    setTurnGunRight(360 * gene.value);
+                    break;
+                case 5:
+                    setFire(5 * gene.value);
+                    break;
+                case 6:
+                    setTurnGunLeft(360 * gene.value);
+                    break;
+                case 7:
+                    System.out.println("7");
+                    //setDistance(10 * gene.value);
+                    break;
+                case 8:
+                    setMaxVelocity(5 * gene.value);
+                    break;
+            }
+        }
+    }
+
 }
