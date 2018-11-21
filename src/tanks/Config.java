31aa41abe2aa94d4848c2a6c0e328575508c21e4
Šárka Chwastková <chwastkova.s@gmail.com>
2018-11-21 18:35:55 +0100

    vraceni percRun, percOnHit, percOnScanned

--- src/tanks/Config.java
+++ src/tanks/Config.java
@@ -3,12 +3,15 @@ package tanks;
 public class Config {
 
 
-    private static int popSize  = 100;
-    private static int iter  = 1000;
+    private static int popSize = 100;
+    private static int iter = 1000;
     private static double percBest = 0.02;
     private static double percCros = 0.74;
     private static double percNew = 0.24;
     public static int numOfGenes = 5;
+    public static double percRun = 0.2;
+    public static double percOnHit = 0.4;
+    public static double percOnScanned = 0.4;
 
 
     public static int getIter() {
@@ -27,15 +30,18 @@ public class Config {
         return percNew;
     }
 
-    public static double getPercRun(){
+    public static double getPercRun() {
         return percRun;
     }
-    public static double getPercOnHit(){
+
+    public static double getPercOnHit() {
         return percOnHit;
     }
-    public static double getPercOnScanned(){
+
+    public static double getPercOnScanned() {
         return percOnScanned;
     }
+
     public static int getNumOfGenes() {
         return numOfGenes;
     }
